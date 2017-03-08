910f0c09ebe9932a4a76216b6205f52c41267e8e
suxiongit <suxiongit@qq.com>
2017-03-08 19:17:42 +0800

    add Array.prototype.forEach

--- extensions/prototype.js
+++ extensions/prototype.js
@@ -5,24 +5,75 @@
 
 // Array 类型扩展
 
-/**
- * 数组中是否存在指定的值
- * @param obj
- * @returns {boolean}
- */
-Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i --) {
-        if (this[i] === obj) {
-            return true;
+if (!Array.prototype.contains) {
+    /**
+     * 数组中是否存在指定的值
+     * @param obj
+     * @returns {boolean}
+     */
+    Array.prototype.contains = function(element) {
+        var i = this.length;
+        while (i --) {
+            if (this[i] === element) {
+                return true;
+            }
         }
+        return false;
     }
-    return false;
+
+    // 示例
+    // var countries = ['美国', '俄罗斯', '英国', '法国', '中国'];
+    // console.log(countries.contains('中国'));//true
+    // console.log(countries.contains('日本'));//false
 }
 
-// 示例
-// var countries = ['美国', '俄罗斯', '英国', '法国', '中国'];
-// console.log(countries.contains('中国'));//true
-// console.log(countries.contains('日本'));//false
+if (!Array.prototype.forEach) {
+    /**
+     * 数组循环遍历
+     * （让IE兼容forEach方法）
+     * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/forEach
+     * @param callback
+     */
+    Array.prototype.forEach = function(callback/*, thisArg*/) {
+        var T, k;
+        if (this == null) {
+            throw new TypeError('this is null or not defined');
+        }
 
+        var O = Object(this);
+        var len = O.length >>> 0; // Hack to convert O.length to a UInt32
+
+        if (typeof callback !== 'function') { // {}.toString.call(callback) != '[object Function]'
+            throw new TypeError(callback + ' is not a function');
+        }
+
+        if (arguments.length > 1) { // thisArg
+            T = arguments[1]; // T = thisArg
+        }
+
+        k = 0;
+        while (k < len) {
+            var kValue;
+            if (k in O) {
+                kValue = O[k];
+                callback.call(T, kValue, k, O);
+            }
+            k++;
+        }
+    }
+
+    // 示例
+    // var countries = [];
+    // countries.push('美国');
+    // countries.push('俄罗斯');
+    // countries.push('英国');
+    // countries.push('法国');
+    // countries.push('中国');
+    // countries.forEach(function(value) {
+    //     console.log(value);
+    // });
+    // countries.forEach(function(value, index, array) {
+    //     console.log(value + ' ' + index + ' ' + array);
+    // });
+}
 
--- prototype.test.js
+++ prototype.test.js
@@ -5,10 +5,26 @@
 
 require('./extensions/prototype.js');
 
-function testContains() {
+/**
+ * 数组中是否存在指定的值
+ */
+function testArrayContains() {
     var countries = ['美国', '俄罗斯', '英国', '法国', '中国'];
     console.log(countries.contains('中国'));//true
     console.log(countries.contains('日本'));//false
 }
-testContains();
+// testArrayContains();
 
+
+function testArrayForEach() {
+    var countries = [];
+    countries.push('美国');
+    countries.push('俄罗斯');
+    countries.push('英国');
+    countries.push('法国');
+    countries.push('中国');
+    countries.forEach(function(item) {
+        console.log(item);
+    });
+}
+// testArrayForEach();
