c60cce43578fb85c743202e56e209769d4e23809
suxiongit <suxiongit@qq.com>
2017-03-09 11:39:51 +0800

    add Date.prototype.format

--- extensions/prototype.js
+++ extensions/prototype.js
@@ -77,3 +77,40 @@ if (!Array.prototype.forEach) {
     // });
 }
 
+
+// Date 类型扩展
+
+if (!Date.prototype.format) {
+    /**
+     * 日期时间格式化
+     * @param format
+     * @returns {*}
+     */
+    Date.prototype.format = function(format) {
+        var date = {
+            "M+" : this.getMonth() + 1,
+            "d+" : this.getDate(),
+            "h+" : this.getHours(),
+            "m+" : this.getMinutes(),
+            "s+" : this.getSeconds(),
+            "q+" : Math.floor((this.getMonth() + 3) / 3),
+            "S+" : this.getMilliseconds()
+        };
+        if (/(y+)/i.test(format)) {
+            format = format.replace(RegExp.$1, (this.getFullYear() + '')
+                .substr(4 - RegExp.$1.length));
+        }
+        for ( var k in date) {
+            if (new RegExp("(" + k + ")").test(format)) {
+                format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k]
+                    : ("00" + date[k]).substr(("" + date[k]).length));
+            }
+        }
+        return format;
+    }
+
+    // 示例
+    // var newDate = new Date('2014-07-10 10:21:12');
+    // console.log(newDate.format('yyyy年MM月dd日 h时m分s秒'));
+}
+
