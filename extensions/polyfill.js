dccf85fb7381032fa9b3381b6d6296a068bd6bf8
suxiongit <suxiongit@qq.com>
2017-04-21 21:13:00 +0800

    update String.format|String.prototype.format

--- extensions/polyfill.js
+++ extensions/polyfill.js
@@ -732,7 +732,7 @@ if (!String.format) {
     String.format = function(format) {
         var args = Array.prototype.slice.call(arguments, 1);
         return format.replace(/\{(\d+)\}/g, function(match, i) {
-            return args[i] == 'undefined' ? match : args[i];
+            return (typeof args[i] == 'undefined') ? match : args[i];
         });
     };
 
@@ -749,7 +749,7 @@ if (!String.prototype.format) {
     String.prototype.format = function() {
         var args = Array.prototype.slice.call(arguments, 0);
         return this.replace(/\{(\d+)\}/g, function(match, i) {
-            return args[i] == 'undefined' ? match : args[i];
+            return (typeof args[i] == 'undefined') ? match : args[i];
         });
     };
 
