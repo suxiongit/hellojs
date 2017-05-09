baa01bd8fa715e0232f550106bdd7b885ac1a645
suxiongit <suxiongit@qq.com>
2017-05-09 11:18:41 +0800

    del getAge 根据日期计算年龄

--- extensions/common.js
+++ extensions/common.js
@@ -4,59 +4,6 @@
  * @date 2017/3/9
  */
 
-// Math.random() 随机数
-
-/**
- * 获取0-1之间的随机数（带小数点）
- * @returns {number}
- */
-function getRandom() {
-    return Math.random();
-}
-
-/**
- * 获取min到max之间的随机数（带小数点）
- * @param min
- * @param max
- * @returns {*}
- */
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-/**
- * 获取min到max之间的随机整数
- * @param min
- * @param max
- * @returns {*}
- */
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-/**
- * 获取min到max之间的随机整数（包含max）
- * @param min
- * @param max
- * @returns {*}
- */
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// 示例
-// for (var i = 0; i < 1000; i ++) {
-    // console.log(getRandom());
-    // console.log(getRandomArbitrary(1, 9));
-    // console.log(getRandomInt(1, 9));
-    // console.log(getRandomIntInclusive(1, 9));
-// }
-
-
 /**
  * 获取Url参数值
  * @param name
@@ -128,25 +75,6 @@ function genNonDuplicateID(length) {
 // for (var i = 0; i < 10; i ++) {
 //     console.log(genNonDuplicateID());
 // }
-
-/**
- * 根据日期计算年龄
- * @param dateString
- * @returns {number}
- */
-function getAge(dateString) {
-    var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    return age;
-}
-
-// 示例
-// console.log(getAge('1989,02,17'));
 
 /**
  * 获取浏览器类型及主版本
