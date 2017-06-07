e4864da9d782cd1a65a0652803baa21a42a5dff7
suxiongit <suxiongit@qq.com>
2017-06-07 14:37:32 +0800

    add addFavorite 加入收藏夹 & setHomepage 设为首页

--- extensions/frontend.js
+++ extensions/frontend.js
@@ -5,20 +5,6 @@
  */
 
 /**
- * 获取Url参数值
- * @param name
- * @returns {null}
- */
-function getUrlParam(name) {
-    // var reg = new RegExp('(^|&)'+ name +'=([^&]*)(&|$)');//构造一个含有目标参数的正则表达式对象
-    // var r = window.location.search.substr(1).match(reg);//匹配目标参数
-    // if (r != null) return unescape(r[2]); return null; //返回参数值
-
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}
-
-/**
  * 判断是否PC访问
  * @returns {boolean}
  */
@@ -41,6 +27,20 @@ function isPC() {
 // } else {
 //     console.log('移动设备');
 // }
+
+/**
+ * 获取Url参数值
+ * @param name
+ * @returns {null}
+ */
+function getUrlParam(name) {
+    // var reg = new RegExp('(^|&)'+ name +'=([^&]*)(&|$)');//构造一个含有目标参数的正则表达式对象
+    // var r = window.location.search.substr(1).match(reg);//匹配目标参数
+    // if (r != null) return unescape(r[2]); return null; //返回参数值
+
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
 
 /**
  * 获取浏览器类型及主版本
@@ -101,8 +101,44 @@ function getCookie(name) {
     var arr = document.cookie.match(new RegExp('(^| )' + name + '=([^;]*)(;|$)'));
     if (arr != null) return unescape(arr[2]);
     return null
-};
+}
 
+/**
+ * 加入收藏夹
+ * @param sURL
+ * @param sTitle
+ * @constructor
+ */
+function addFavorite(sURL, sTitle) {
+    try {
+        window.external.addFavorite(sURL, sTitle)
+    } catch (e) {
+        try {
+            window.sidebar.addPanel(sTitle, sURL, "")
+        } catch (e) {
+            alert("加入收藏失败，请使用Ctrl+D进行添加")
+        }
+    }
+}
 
-
+/**
+ * 设为首页
+ * @param homeurl
+ */
+function setHomepage(homeurl) {
+    if (document.all) {
+        document.body.style.behavior = 'url(#default#homepage)';
+        document.body.setHomePage(homeurl)
+    } else if (window.sidebar) {
+        if (window.netscape) {
+            try {
+                netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect")
+            } catch (e) {
+                alert("该操作被浏览器拒绝，如果想启用该功能，请在地址栏内输入about:config,然后将项 signed.applets.codebase_principal_support 值该为true");
+            }
+        }
+        var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch);
+        prefs.setCharPref('browser.startup.homepage', homeurl)
+    }
+}
 
